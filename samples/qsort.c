bf0fe554e8bb0ed7b16af6b844ba8e25a291c4c3
Kray-G <nagai.kazuya@gmail.com>
2019-11-11 14:29:39 +0900

    issue #17: updated sample for qsort.

--- samples/qsort.c
+++ samples/qsort.c
@@ -6,13 +6,22 @@ int cmp(const void *p, const void *q)
     return *(int*)p - *(int*)q;
 }
 
-main()
-{
-    int values[] = { 49, 2, 180, 23, 77, 13, 29, 88 };
-    int n, i;
+#define N 20
+int a[N];
 
-    n = sizeof(values)/sizeof(int);
-    qsort(values, n, sizeof(int), cmp);
-    for (i = 0; i < n; i++)
-        printf("values[%d] = %d\n", i, values[i]);
+int main(void)
+{
+    int i;
+
+    printf("Before:");
+    for (i = 0; i < N; i++) {
+        a[i] = rand() / (RAND_MAX / 100 + 1);
+        printf(" %2d", a[i]);
+    }
+    printf("\n");
+    qsort(a, sizeof(a)/sizeof(a[0]), sizeof(a[0]), cmp);
+    printf("After: ");
+    for (i = 0; i < N; i++) printf(" %2d", a[i]);
+    printf("\n");
+    return 0;
 }
